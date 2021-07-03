4182712db06795cedfa66a1bf2edad5c0aa11d38
Osinaike Evans <the.evans.osinaike@gmail.com>
2021-07-03 14:08:24 +0100

    simplified lesson 4 concept 3 code

--- AWS ML Foundations/Intro to Python Programming/L4 - C3.py
+++ AWS ML Foundations/Intro to Python Programming/L4 - C3.py
@@ -1,13 +1,13 @@
 points = 174  # use this input to make your submission
 
 # write your if statement here
-if 1 <= points <= 50:
+if points <= 50:
     result = "Congratulations! You won a wooden rabbit!"
-elif 51 <= points <= 150:
+elif points <= 150:
     result = "Oh dear, no prize this time."
-elif 151 <= points <= 180:
+elif points <= 180:
     result = "Congratulations! You won a wafer-thin mint!"
-elif 181 <= points <= 200:
+else:
     result = "Congratulations! You won a Penguin!"
 
 print(result)
